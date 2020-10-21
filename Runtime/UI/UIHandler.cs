9b83ae69a32bfcd3b0151f6e9cfebcdb81f0f524
AltaNox <altanoxgames@gmail.com>
2020-10-21 13:30:54 -0500

    Initial files

---
+++ Runtime/UI/UIHandler.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Nox.Core
+{
+	public class UIHandler : MonoBehaviour
+	{
+		private void Awake()
+		{
+			screenStack = new Stack<UILayer>();
+			availableScreens = new Dictionary<Type, UILayer>();
+			UILayer[] screens = GetComponentsInChildren<UILayer>(true);
+			for (int i = 0; i < screens.Length; i++)
+			{
+				Type screenType = screens[i].GetType();
+				availableScreens.Add(screenType, screens[i]);
+				screens[i].gameObject.SetActive(false);
+				screens[i].handler = this;
+			}
+
+			foreach (UILayer s in availableScreens.Values)
+				s.OnScreenStart();
+		}
+
+		private Dictionary<Type, UILayer> availableScreens;
+		private Stack<UILayer> screenStack;
+
+		public Type GetCurrentScreenType()
+		{
+			return screenStack.Count > 0 ? screenStack.Peek().GetType() : null;
+		}
+
+		public void Show(Type screenType, float upDelay = 0)
+		{
+			UILayer newScreen = availableScreens[screenType];
+			//Check if the stack is not empty
+			if (screenStack.Count != 0 && newScreen.hidePrevious)
+				TakeScreenDown(screenStack.Peek());
+			screenStack.Push(newScreen);
+			StartCoroutine(DelayedUp(newScreen, upDelay));
+		}
+
+		public void Show<T>(float upDelay = 0) where T : UILayer
+		{
+			Type screenType = typeof(T);
+			Show(screenType, upDelay);
+		}
+
+		public void ImmediateShow(Type screenType)
+		{
+			UILayer newScreen = availableScreens[screenType];
+			if (screenStack.Count != 0 && newScreen.hidePrevious)
+				TakeScreenDown(screenStack.Peek());
+			screenStack.Push(newScreen);
+			PutScreenUp(newScreen);
+		}
+
+		public void ImmediateShow<T>() where T : UILayer
+		{
+			Type screenType = typeof(T);
+			ImmediateShow(screenType);
+		}
+
+		public void Hide(float downDelay = 0, float upDelay = 0)
+		{
+			StartCoroutine(DelayedDown(screenStack.Pop(), downDelay));
+			if (screenStack.Count == 0) return;
+			StartCoroutine(DelayedUp(screenStack.Peek(), upDelay));
+		}
+
+		public void ImmediateHide()
+		{
+			TakeScreenDown(screenStack.Pop());
+			if (screenStack.Count == 0) return;
+			PutScreenUp(screenStack.Peek());
+		}
+
+		public bool ScreenIsUp(Type screenType)
+		{
+			return availableScreens[screenType].gameObject.activeSelf;
+		}
+
+		public void Clear()
+		{
+			int count = screenStack.Count;
+			for (int i = 0; i < count; i++)
+				TakeScreenDown(screenStack.Pop());
+		}
+
+		private void TakeScreenDown(UILayer screen)
+		{
+			screen.gameObject.SetActive(false);
+			screen.OnScreenDown();
+		}
+
+		private void PutScreenUp(UILayer screen)
+		{
+			screen.gameObject.SetActive(true);
+			screen.OnScreenUp();
+		}
+
+		private IEnumerator DelayedUp(UILayer screen, float delay)
+		{
+			yield return new WaitForSeconds(delay);
+			PutScreenUp(screen);
+		}
+
+		private IEnumerator DelayedDown(UILayer screen, float delay)
+		{
+			yield return new WaitForSeconds(delay);
+			TakeScreenDown(screen);
+		}
+	}
+}
---
+++ Runtime/UI/UILayer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Nox.Core
+{
+	public abstract class UILayer : MonoBehaviour
+	{
+		public UIHandler handler { protected get; set; }
+
+		//Should the previous screen get hidden when showing this screen?
+		public bool hidePrevious;
+
+		/// <summary>
+		/// Callback method executed for every screen.
+		/// </summary>
+		public virtual void OnScreenStart() { }
+
+		//REMINDER: Never use ScreenManager.Show inside OnScreenUp, because if Hide is used on that other screen, this method it will repromt it
+		//making an ugly loop
+		//Gets called every time a screen is shown
+		public virtual void OnScreenUp() { }
+		//Gets called every time a screen gets hidden
+		public virtual void OnScreenDown() { }
+
+		//Gets called right before the screen manager loads a new scene
+		public virtual void OnBeforeLoadScene() { }
+
+		// Many screens have a back button so a method for that is included in this class to avoid rewriting it
+		// in every child class
+		public virtual void Back() { handler.ImmediateHide(); }
+	}
+}
