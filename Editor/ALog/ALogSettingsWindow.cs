88fc62f6dc4c795d566aefea99c5261dbf9f2d7d
AltaNox <altanoxgames@gmail.com>
2020-10-21 14:20:13 -0500

    ALog initial files

---
+++ Editor/ALog/ALogSettingsWindow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Nox.Core
+{
+	public class ALogSettingsWindow : EditorWindow
+	{
+		[MenuItem("Window/Nox/ALog Settings")]
+		private static void ShowWindow()
+		{
+			ALogSettingsWindow window = GetWindow<ALogSettingsWindow>();
+
+			window.titleContent = new GUIContent("ALog Settings");
+
+			window.Show();
+		}
+	}
+}
